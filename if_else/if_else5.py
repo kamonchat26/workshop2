79bea327bcf8050bc13132557bcc12fa31f56325
kamonchat26 <kamonchat2607@gmail.com>
2021-02-06 10:17:52 +0700

    Add new folder name if_else have 5 file

---
+++ if_else/if_else5.py
@@ -0,0 +1,9 @@
+a = 2
+b = 330
+
+max = a if a > b else b
+
+print(max)
+
+
+# Output :  330
---
+++ if_else/if_else6.py
@@ -0,0 +1,8 @@
+a = 200
+b = 33
+c = 500
+
+if a > b and C > a:
+    print("Both conditions are True")
+
+# Output :  "Both conditions are True"
