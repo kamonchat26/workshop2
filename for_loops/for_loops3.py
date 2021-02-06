46ca07aaa767bdeb11ba9c6fbe22e4caec8e8afe
kamonchat26 <kamonchat2607@gmail.com>
2021-02-06 10:20:56 +0700

    Add new folder name for_loops have 6 file

---
+++ for_loops/for_loops3.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    if fruit == "banana":
+        break
+    print(fruit)
+
+
+# Output:
+# apple
---
+++ for_loops/for_loops5.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+else:
+    print("Finally finished!")
+
+# Output:
+# apple
+# banana
+# cherry
+# Finally finished!
